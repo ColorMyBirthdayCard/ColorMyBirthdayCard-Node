6ebd1799a520b2e7af1fea9baaf0cafbdc40c44b
Jung SeokYoung <thrdud0423@gmail.com>
2022-12-03 21:42:42 +0900

    1
    1

--- routes/router.js
+++ routes/router.js
@@ -72,11 +72,13 @@ router.post('/login', async function(req, res) {
         // done(err)가 처리된 경우
         if (authError) {
            console.error(authError);
+           console.log('auth error')
            return res.status(401)
         }
         //비번 일치 X or 존재 X user
         if (!user) {
            // done()의 3번째 인자 { message: '비밀번호가 일치하지 않습니다.' }가 실행
+           console.log("not exist user")
            return res.status(401).send(info.message);
         }
     
@@ -86,20 +88,22 @@ router.post('/login', async function(req, res) {
            // 만일 done(err) 가 됬다면,
            if (loginError) {
               console.error(loginError);
+              console.log("login.errr")
               return res.status(401);
            }
            // done(null, user)로 로직이 성공적이라면, 세션에 사용자 정보를 저장해놔서 로그인 상태가 된다.
-            console.log("login success")
             const userInfor = {
                 sessionId: req.sessionID,
                 userId: req.session.passport.user
-	    }
+	         }
+            console.log("login success")
+
             return res.send(userInfor);
         });
-     })(req, res); //! 미들웨어 내의 미들웨어에는 콜백을 실행시키기위해 (req, res, next)를 붙인다.
+     })(req, res); 
 });
 
-router.get('/logout')
+// router.get('/logout')
 
 router.get('/test', function(req, res) {
 	console.log(req.isAuthenticated())
